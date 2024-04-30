f72205542cb41cb7c584211d095eb9eff6512b26
Aryan <aryansinghal207@gmail.com>
2024-04-30 09:43:00 +0530

    g

--- portfolio.js
+++ portfolio.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
   
     // typing text animation script
     var typed = new Typed(".typing", {
-      strings: ["Developer","Sportsman","Designer","Gymrat"],
+      strings: ["Developer","Sportsman","Designer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
     });
   
     var typed = new Typed(".typing-2", {
-      strings: ["Developer","Sportsman","Designer","Gymrat"],
+      strings: ["Developer","Sportsman","Designer"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true,
